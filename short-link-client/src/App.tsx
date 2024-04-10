b74794b25ec5a7b58950beb470eba1d10fa91515
Prabhudatta Barik <thexas21@gmail.com>
2024-04-11 01:25:38 +0530

    Minor changes

--- short-link-client/src/App.tsx
+++ short-link-client/src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Form from './components/Form';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Form from './components/Form.tsx';
 
 function App () {
     return (
     <Router>
         <div className="App">
-            <div className="auth-wrapper">
-                <div className="auth-inner">
-                    <Switch>
-                        <Route path='/' component={Form}/>
-                        <Route path='/app' component={Form}/>
-                    </Switch>
+            <div className="text-center mt-0 leading-none pb-20">
+                <div className="w-auto m-auto bg-white-300 shadow-lg pt-40 pr-55 pb-45 pl-55 rounded-md transition-all">
+                    <Routes>
+                        <Route path='/' element={<Form/>}/>
+                        <Route path='/app' element={<Form/>}/>
+                    </Routes>
                 </div>
             </div>
         </div>
